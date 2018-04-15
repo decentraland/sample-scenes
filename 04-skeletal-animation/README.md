eb5db5d140d1e1cf19a2531ecdcdd6d570ef62a9
Christopher D. Chapman <chris@m3mnoch.com>
2018-04-15 10:39:20 -0700

    swimming shark model

--- 04-skeletal-animation/README.md
+++ 04-skeletal-animation/README.md
@@ -1,4 +1,6 @@
-https://github.com/decentraland/client/tree/master/public/test-parcels/0.27.skeletal-animation
+In your scenes, you can load up an interactive GLTF model and trigger its animations. This is an example of how to do that.
+
+[![shark swimming in the air](https://img.youtube.com/vi/Xf0D2hROq_g/0.jpg)](https://www.youtube.com/watch?v=Xf0D2hROq_g)
 
 Once you've got the CLI installed, to view the scene, you only need to type `dcl preview` at the command prompt. It will open the scene in a new browser tab automatically.
 
---
+++ 04-skeletal-animation/scene.tsx
@@ -0,0 +1,40 @@
+import { Component, createElement } from "metaverse-api";
+
+export default class SharkAnimation extends Component {
+    state = {
+        bitestate: false,
+        dancestate: false
+    };
+
+    async componentDidMount() {
+        this.eventSubscriber.on(`muhshark_click`, () => this.clickedOnShark());
+    }
+
+    clickedOnShark() {
+        this.setState({ bitestate: !this.state.bitestate });
+    }
+
+    async render() {
+        return (
+            <a-scene>
+                <a-gltf-model
+                    id="supershark"
+                    position={{ x: 5, y: 3, z: -5 }}
+                    scale={0.5}
+                    src="models/shark_anim.gltf"
+                    skeletalAnimation={
+                        this.state.bitestate
+                            ? [
+                                  { clip: "shark_skeleton_bite", playing: false },
+                                  { clip: "shark_skeleton_swim", weight: 0.2, playing: true }
+                              ]
+                            : [
+                                  { clip: "shark_skeleton_bite", playing: true, loop: true },
+                                  { clip: "shark_skeleton_swim", weight: 1.0, playing: true }
+                              ]
+                    }
+                />
+            </a-scene>
+        );
+    }
+}

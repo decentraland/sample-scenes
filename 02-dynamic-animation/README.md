516476689cf095201158d1a886d9337cc6ead6a8
Christopher D. Chapman <chris@m3mnoch.com>
2018-04-18 12:36:24 -0700

    updated to remove 'a-'

--- 02-dynamic-animation/README.md
+++ 02-dynamic-animation/README.md
@@ -1,5 +1,5 @@
 With this Dynamic Animation, we're demonstrating how to employ simple data binding to objects in your scene. Translation, rotation, and scale are all properties you can bind to state values.
 
-Once you've got the CLI installed, to view the scene, you only need to type `dcl preview` at the command prompt. It will open the scene in a new browser tab automatically.
+Once you've got the CLI installed, to view the scene, you only need to type `dcl preview` at the command prompt. It will make sure the dependencies are taken care of, and then open the scene in a new browser tab automatically.
 
 You can learn more about our JSX-style lifecycle and rendering in our documentation: https://docs.decentraland.org/docs
--- 02-dynamic-animation/scene.tsx
+++ 02-dynamic-animation/scene.tsx
@@ -1,61 +1,51 @@
-// RollerCoaster
+import { createElement, ScriptableScene } from 'metaverse-api'
 
-import { Component, createElement } from "metaverse-api";
+const networkHz = 6
+const interval = 1000 / networkHz
 
-const networkHz = 6;
-const interval = 1000 / networkHz;
+export default class RollerCoaster extends ScriptableScene<any, { time: number }> {
+  state = { time: 0 }
 
-// The Component class is a React-style component.
-export default class RollerCoaster extends Component<any, { time: number }> {
-    state = { time: 0 };
+  timeout = setInterval(() => {
+    this.setState({
+      time: performance.now() * 0.0001
+    })
+  }, interval)
 
-    timeout = setInterval(() => {
-        // As with React, we are tracking state changes and
-        // telling the SDK when it's time to re-render
-        this.setState({
-            time: performance.now() * 0.0001
-        });
-    }, interval);
+  sceneWillUnmount() {
+    clearInterval(this.timeout)
+  }
 
-    componentWillUnmount() {
-        clearInterval(this.timeout);
-    }
+  async render() {
+    const { time } = this.state
 
-    async render() {
-        const { time } = this.state;
+    const size = 2
 
-        const size = 2;
+    const x = Math.cos(time) * Math.cos(time) * size
+    const y = Math.cos(time) * Math.sin(time) * size
+    const z = Math.sin(time) * size
 
-        // Here, we're just running some fun, wave animations
-        const x = Math.cos(time) * Math.cos(time) * size;
-        const y = Math.cos(time) * Math.sin(time) * size;
-        const z = Math.sin(time) * size;
+    return (
+      <scene>
+        <entity position={{ x: 5, y: 4, z: 5 }}>
+          <entity
+            id="train"
+            position={{ x, y, z }}
+            rotation={{ x: Math.cos(time) * 40, y: Math.sin(time) * 40, z: 0 }}
+            transition={{
+              position: { duration: interval },
+              rotation: { duration: interval }
+            }}
+          >
+            <box position={{ x: 0, y: -1, z: 0 }} color="black" scale={{ x: 3, y: 0.4, z: 5 }} />
+            <box position={{ x: 1.5, y: 0, z: 0 }} color="red" scale={{ x: 0.2, y: 1, z: 5 }} />
+            <box position={{ x: -1.5, y: 0, z: 0 }} color="yellow" scale={{ x: 0.2, y: 1, z: 5 }} />
 
-        // This is the API's secret sauce:  JSX Data Binding
-        return (
-            <a-scene>
-                <a-entity position={{ x: 5, y: 4, z: 5 }}>
-                    <a-entity
-                        id="train"
-                        position={{ x, y, z }}
-                        rotation={{
-                            x: Math.cos(time) * 40,
-                            y: Math.sin(time) * 40,
-                            z: 0
-                        }}
-                        transition={{
-                            position: { duration: interval },
-                            rotation: { duration: interval }
-                        }}
-                    >
-                        <a-box position={{ x: 0, y: -1, z: 0 }} color="black" scale={{ x: 3, y: 0.4, z: 5 }} />
-                        <a-box position={{ x: 1.5, y: 0, z: 0 }} color="red" scale={{ x: 0.2, y: 1, z: 5 }} />
-                        <a-box position={{ x: -1.5, y: 0, z: 0 }} color="yellow" scale={{ x: 0.2, y: 1, z: 5 }} />
-                        <a-box position={{ x: 0, y: 0, z: 2.5 }} color="green" scale={{ x: 3, y: 1, z: 0.2 }} />
-                        <a-box position={{ x: 0, y: 0, z: -2.5 }} color="blue" scale={{ x: 3, y: 1, z: 0.2 }} />
-                    </a-entity>
-                </a-entity>
-            </a-scene>
-        );
-    }
+            <box position={{ x: 0, y: 0, z: 2.5 }} color="green" scale={{ x: 3, y: 1, z: 0.2 }} />
+            <box position={{ x: 0, y: 0, z: -2.5 }} color="blue" scale={{ x: 3, y: 1, z: 0.2 }} />
+          </entity>
+        </entity>
+      </scene>
+    )
+  }
 }

e7a616b15bdaeb9e273fd709dd500407cf63354a
belohlavek <dani@decentraland.org>
2018-04-18 18:08:56 -0300

    fixed door click

--- 03-interactive-door/scene.tsx
+++ 03-interactive-door/scene.tsx
@@ -1,31 +1,31 @@
 import { createElement, ScriptableScene } from "metaverse-api";
 
 export default class HouseScene extends ScriptableScene {
-    state = {
-        isDoorClosed: false
+  state = {
+    isDoorClosed: false
+  };
+
+  sceneDidMount() {
+    this.eventSubscriber.on("door_click", () => {
+      this.setState({ isDoorClosed: !this.state.isDoorClosed });
+    });
+  }
+
+  async render() {
+    const doorRotation = {
+      x: 0,
+      y: this.state.isDoorClosed ? 0 : 90,
+      z: 0
     };
 
-    componentDidMount() {
-        this.eventSubscriber.on("door_click", () => {
-            this.setState({ isDoorClosed: !this.state.isDoorClosed });
-        });
-    }
-
-    async render() {
-        const doorRotation = {
-            x: 0,
-            y: this.state.isDoorClosed ? 0 : 90,
-            z: 0
-        };
-
-        return (
-            <scene position={{ x: 5, y: 0, z: 5 }}>
-                <entity rotation={doorRotation} transition={{ rotation: { duration: 1000, timing: "ease-in" } }}>
-                    <box id="door" scale={{ x: 1, y: 2, z: 0.05 }} position={{ x: 0.5, y: 1, z: 0 }} color="brown" />
-                </entity>
-                <box position={{ x: 2, y: 1, z: 0 }} scale={{ x: 2, y: 2, z: 0.05 }} color="blue" />
-                <box position={{ x: -1, y: 1, z: 0 }} scale={{ x: 2, y: 2, z: 0.05 }} color="blue" />
-            </scene>
-        );
-    }
+    return (
+      <scene position={{ x: 5, y: 0, z: 5 }}>
+        <entity rotation={doorRotation} transition={{ rotation: { duration: 1000, timing: "ease-in" } }}>
+          <box id="door" scale={{ x: 1, y: 2, z: 0.05 }} position={{ x: 0.5, y: 1, z: 0 }} color="brown" />
+        </entity>
+        <box position={{ x: 2, y: 1, z: 0 }} scale={{ x: 2, y: 2, z: 0.05 }} color="blue" />
+        <box position={{ x: -1, y: 1, z: 0 }} scale={{ x: 2, y: 2, z: 0.05 }} color="blue" />
+      </scene>
+    );
+  }
 }
